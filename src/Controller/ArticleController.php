64d4ab42967f1da57bec23795b9fcd555e6f86bb
BenoitCrozemarie <85104535+BenoitCrozemarie@users.noreply.github.com>
2021-09-15 10:14:53 +0200

    FEAT: dashboard OK

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,25 +3,28 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController{
 
-    /**
-     * @Route("/user/dashboard/{idUser}", name="dashboard_user")
-     */
-    public function dashboard($idUser,ArticleRepository $articleRepository){
+    
+    #[Route('/user/dashboard/{idUser}', name: 'dashboard_user')]
+    public function dashboard($idUser,ArticleRepository $articleRepository,UserRepository $userRepository){
 
-        $articles = $articleRepository->findBy(['User']);
+        
+        $user = $userRepository->find($idUser);
+       
+        $articles = $articleRepository->findBy(['user' => $user]);
 
         return $this->render('user/dashboard.html.twig', [
+            'user' => $user,
             'articles' => $articles
         ]);
-
     }
-    /**
-     * @Route("/user/dashboard/{idUser}/create", name="article_create")
-     */
+
+    #[Route('/user/dashboard/{idUser}/create', name: 'article_create')]
     public function createArticle($idUser,ArticleRepository $articleRepository){
 
         $articles = $articleRepository->findAll();
@@ -29,11 +32,9 @@ class ArticleController extends AbstractController{
         return $this->render('user/dashboard.html.twig', [
             'articles' => $articles
         ]);
-
     }
-    /**
-     * @Route("/user/dashboard/{idUser}/update/{idArticle}", name="article_modify")
-     */
+
+    #[Route('/user/dashboard/{idUser}/update/{idArticle}', name: 'article_modify')]
     public function updateArticle($idUser,$idArticle, ArticleRepository $articleRepository){
 
         $articles = $articleRepository->findAll();
@@ -41,11 +42,9 @@ class ArticleController extends AbstractController{
         return $this->render('user/dashboard.html.twig', [
             'articles' => $articles
         ]);
-
     }
-    /**
-     * @Route("/user/dashboard/{idUser}/delete/{idArticle}", name="article_delete")
-     */
+
+    #[Route('/user/dashboard/{idUser}/delete/{idArticle}', name: 'article_delete')]
     public function deleteArticle($idUser,$idArticle,ArticleRepository $articleRepository){
 
         $articles = $articleRepository->findAll();
@@ -53,6 +52,5 @@ class ArticleController extends AbstractController{
         return $this->render('user/dashboard.html.twig', [
             'articles' => $articles
         ]);
-
     }
 }
