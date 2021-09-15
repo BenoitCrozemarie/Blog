1cc1895c79a8c3826ecb3f145308cdebde70bcc0
BenoitCrozemarie <85104535+BenoitCrozemarie@users.noreply.github.com>
2021-09-15 11:04:35 +0200

    FEAT:Create/Update/Delete Article ok

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,20 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController extends AbstractController{
+class ArticleController extends AbstractController
+{
 
-    
+
     #[Route('/user/dashboard/{idUser}', name: 'dashboard_user')]
-    public function dashboard($idUser,ArticleRepository $articleRepository,UserRepository $userRepository){
+    public function dashboard($idUser, ArticleRepository $articleRepository, UserRepository $userRepository)
+    {
 
-        
+
         $user = $userRepository->find($idUser);
-       
+
         $articles = $articleRepository->findBy(['user' => $user]);
 
         return $this->render('user/dashboard.html.twig', [
@@ -25,32 +32,75 @@ class ArticleController extends AbstractController{
     }
 
     #[Route('/user/dashboard/{idUser}/create', name: 'article_create')]
-    public function createArticle($idUser,ArticleRepository $articleRepository){
+    public function createArticle(
+        $idUser,
+        ArticleRepository $articleRepository,
+        UserRepository $userRepository,
+        EntityManagerInterface $entityManager,
+        Request $request
+    ) {
+        $user = $userRepository->find($idUser);
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
 
-        $articles = $articleRepository->findAll();
+        if ($form->isSubmitted() && $form->isValid()) {
+            //dd('submit');
+            $article->setUser($user);
+            $entityManager->persist($article);
+            $entityManager->flush();
 
-        return $this->render('user/dashboard.html.twig', [
-            'articles' => $articles
+            return $this->redirectToRoute('dashboard_user', ['idUser' => $idUser]);
+        }
+
+        return $this->render('user/formArticle.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 
     #[Route('/user/dashboard/{idUser}/update/{idArticle}', name: 'article_modify')]
-    public function updateArticle($idUser,$idArticle, ArticleRepository $articleRepository){
+    public function updateArticle(
+        $idUser,
+        $idArticle,
+        ArticleRepository $articleRepository,
+        UserRepository $userRepository,
+        EntityManagerInterface $entityManager,
+        Request $request
+    ) {
+        $user = $userRepository->find($idUser);
+        $article = $articleRepository->find($idArticle);
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
 
-        $articles = $articleRepository->findAll();
+        if ($form->isSubmitted() && $form->isValid()) {
+            //dd('submit');
+            //$article->setUser($user);
+            $entityManager->persist($article);
+            $entityManager->flush();
 
-        return $this->render('user/dashboard.html.twig', [
-            'articles' => $articles
+            return $this->redirectToRoute('dashboard_user', ['idUser' => $idUser]);
+        }
+
+        return $this->render('user/formArticle.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 
     #[Route('/user/dashboard/{idUser}/delete/{idArticle}', name: 'article_delete')]
-    public function deleteArticle($idUser,$idArticle,ArticleRepository $articleRepository){
+    public function deleteArticle(
+        $idUser,
+        $idArticle,
+        UserRepository $userRepository,
+        EntityManagerInterface $entityManager,
+        ArticleRepository $articleRepository
+    ) {
+        $user = $userRepository->find($idUser);
+        $article = $articleRepository->find($idArticle);
 
-        $articles = $articleRepository->findAll();
+        $entityManager->remove($article);
+        $entityManager->flush();
 
-        return $this->render('user/dashboard.html.twig', [
-            'articles' => $articles
-        ]);
+        return $this->redirectToRoute('dashboard_user', ['idUser' => $idUser]);
+
     }
 }
---
+++ src/Form/ArticleType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content',TextareaType::class)
+            ->add('url',TextareaType::class)
+            ->add('submit',SubmitType::class)
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                $article = $event->getData();
+                $form = $event->getForm();
+        
+                // checks if the Product object is "new"
+                // If no data is passed to the form, the data is "null".
+                // This should be considered a new "Product"
+                if (!$article || null === $article->getId()) {
+                    $form->get('title')->setData($article->getTitle());
+                    $form->get('content')->setData($article->getContent());
+                    $form->get('url')->setData($article->getUrl());
+                }
+            });
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data-class'=>Article::class
+        ]);
+    }
+}
